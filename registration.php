69f88bc6e10625ffc877822036c83fe983464a5c
AndyMakrlik <dennyiscz@gmail.com>
2024-05-02 12:08:48 +0200

    Style link

--- registration.php
+++ registration.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/b9aa334442.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
     <title>Registrace</title>
 </head>
 <body>
