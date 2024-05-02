dc38dc01ee2395bed99025aa0a3499ebb5179944
AndyMakrlik <dennyiscz@gmail.com>
2024-05-02 12:05:53 +0200

    Registrace

---
+++ registration.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/b9aa334442.js" crossorigin="anonymous"></script>
+    <title>Registrace</title>
+</head>
+<body>
+    <div class="div">
+        <div class="formular">
+            <h1>Registrace</h1>
+            <form action="registration.php" method="post">
+                <div class="vstupy">
+                    <div class="vstupniPole">
+                        <i class="fa-solid fa-user"></i>
+                        <input type="text" placeholder="Přezdívka">
+                    </div>
+                    <div class="vstupniPole">
+                        <i class="fa-solid fa-envelope"></i>
+                        <input type="email" placeholder="Email">
+                    </div>
+                    <div class="vstupniPole">
+                        <i class="fa-solid fa-key"></i>
+                        <input type="password" placeholder="Heslo">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
