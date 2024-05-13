d96300f72b56134bc9a7d23ba113aebab0bef382
kiko4cz <149481850+kiko4cz@users.noreply.github.com>
2024-05-13 09:00:49 +0200

    Píňďík
    PPPPPPPenis

--- login.php
+++ login.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styleIndex.css">
+    <link rel="stylesheet" href="styleIndexLogin.css">
     <script src="https://kit.fontawesome.com/b9aa334442.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <title>Přihlášení</title>
